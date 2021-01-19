70578f165d65fc728ffcf6141765ee6bcaab1abb
stefanneidhart <56339117+stefanneidhart@users.noreply.github.com>
2021-01-19 14:22:25 +0100

    update

--- metadata.php
+++ metadata.php
@@ -6,80 +6,85 @@
 /**
  * Metadata version
  */
-$sMetadataVersion = '2.0';
+$sMetadataVersion = '2.1';
 
 /**
  * Module information
  */
 $aModule = array(
-	'id' => 'oxidflaechenberechnung',
-	'title' => array(
-		'de' => 'Flächenberechung',
-		'en' => 'Flächenberechung',
+    'id' => 'oxidflaechenberechnung',
+    'title' => array(
+	'de' => 'Flächenberechung',
+	'en' => 'Flächenberechung',
+    ),
+    'description' => array(
+	'de' => '<h2>Flächenberechung</h2>',
+	'en' => '<h2>Flächenberechung</h2>',
+    ),
+    'thumbnail' => 'calc.png',
+    'version' => '1.0.9',
+    'author' => 'Stefan Neidhart',
+    'url' => 'https://www.stefanneidhart.de',
+    'email' => 'info@stefanneidhart.de',
+    'extend' => array(
+	//\OxidEsales\Eshop\Core\ViewConfig::class => \sn\oxidflaechenberechnung\Core\ViewConfig::class,
+	\OxidEsales\Eshop\Application\Model\Article::class => \sn\oxidflaechenberechnung\Model\Article::class,
+	\OxidEsales\Eshop\Application\Model\Basket::class => \sn\oxidflaechenberechnung\Model\Basket::class,
+	\OxidEsales\Eshop\Application\Model\BasketItem::class => \sn\oxidflaechenberechnung\Model\BasketItem::class
+    //\OxidEsales\Eshop\Application\Model\OrderArticle::class => \sn\oxidflaechenberechnung\Model\OrderArticle::class,
+    ),
+    'controllers' => array(
+	'maincontrollerflaechenberechnung' => \sn\oxidflaechenberechnung\Controller\Admin\MainController::class,
+    ),
+    'files' => array(),
+    'templates' => array(
+	'flaechenberechnungadmin.tpl' => 'sn/oxidflaechenberechnung/views/admin/tpl/flaechenberechnungadmin.tpl',
+    ),
+    'blocks' => array(
+	array(
+	    'template' => 'page/details/inc/productmain.tpl',
+	    'block' => 'details_productmain_shortdesc',
+	    'file' => 'views/blocks/page/details/inc/details_productmain_flaechenberechnung.tpl'
 	),
-	'description' => array(
-		'de' => '<h2>Flächenberechung</h2>',
-		'en' => '<h2>Flächenberechung</h2>',
+	array(
+	    'template' => 'page/details/inc/productmain.tpl',
+	    'block' => 'details_productmain_selectlists',
+	    'file' => 'views/blocks/page/details/inc/details_productmain_flaechenberechnungSelectlist.tpl'
 	),
-	'thumbnail' => 'calc.png',
-	'version' => '1.0.9',
-	'author' => 'Stefan Neidhart',
-	'url' => 'https://www.stefanneidhart.de',
-	'email' => 'info@stefanneidhart.de',
-	'extend' => array(
-		//\OxidEsales\Eshop\Core\ViewConfig::class => \sn\oxidflaechenberechnung\Core\ViewConfig::class,
-		\OxidEsales\Eshop\Application\Model\Article::class	=> \sn\oxidflaechenberechnung\Model\Article::class,
-		\OxidEsales\Eshop\Application\Model\Basket::class	=> \sn\oxidflaechenberechnung\Model\Basket::class,  
-		\OxidEsales\Eshop\Application\Model\BasketItem::class	=> \sn\oxidflaechenberechnung\Model\BasketItem::class
-		//\OxidEsales\Eshop\Application\Model\OrderArticle::class => \sn\oxidflaechenberechnung\Model\OrderArticle::class,
+	array(
+	    'template' => 'page/checkout/inc/basketcontents_list.tpl',
+	    'block' => 'checkout_basketcontents_basketitem_persparams',
+	    'file' => 'views/blocks/checkout/inc/checkout_basketcontents_basketitem_persparams.tpl'
 	),
-	'controllers' => array(
-		'maincontrollerflaechenberechnung' => \sn\oxidflaechenberechnung\Controller\Admin\MainController::class,
+	array(
+	    'template' => 'page/checkout/inc/basketcontents_list.tpl',
+	    'block' => 'checkout_basketcontents_basketitem_selectlists',
+	    'file' => 'views/blocks/checkout/inc/checkout_basketcontents_basketitem_selectlists.tpl'
 	),
-	'files' => array(),
-	'templates' => array(
-		'flaechenberechnungadmin.tpl' => 'sn/oxidflaechenberechnung/views/admin/tpl/flaechenberechnungadmin.tpl',
+	array(
+	    'template' => 'page/account/order.tpl',
+	    'block' => 'account_order_history_cart_items',
+	    'file' => 'views/blocks/account/account_order_history_cart_items.tpl'
 	),
-	'blocks' => array(
-		array(
-			'template' => 'page/details/inc/productmain.tpl',
-			'block' => 'details_productmain_shortdesc',
-			'file' => 'views/blocks/page/details/inc/details_productmain_flaechenberechnung.tpl'
-		),
-		array(
-			'template' => 'page/checkout/inc/basketcontents_list.tpl',
-			'block' => 'checkout_basketcontents_basketitem_persparams',
-			'file' => 'views/blocks/checkout/inc/checkout_basketcontents_basketitem_persparams.tpl'
-		),
-		array(
-			'template' => 'page/checkout/inc/basketcontents_list.tpl',
-			'block' => 'checkout_basketcontents_basketitem_selectlists',
-			'file' => 'views/blocks/checkout/inc/checkout_basketcontents_basketitem_selectlists.tpl'
-		),
-		array(
-			'template' => 'page/account/order.tpl',
-			'block' => 'account_order_history_cart_items',
-			'file' => 'views/blocks/account/account_order_history_cart_items.tpl'
-		),
-		array(
-			'template' => 'email/html/order_owner.tpl',
-			'block' => 'email_html_order_owner_basketitem',
-			'file' => 'views/blocks/email/html/email_html_order_owner_basketitem.tpl'
-		),
-		array(
-			'template' => 'email/plain/order_owner.tpl',
-			'block' => 'email_plain_order_ownerbasketitem',
-			'file' => 'views/blocks/email/plain/email_plain_order_ownerbasketitem.tpl'
-		),
-		array(
-			'template' => 'email/html/order_cust.tpl',
-			'block' => 'email_html_order_cust_basketitem',
-			'file' => 'views/blocks/email/html/email_html_order_cust_basketitem.tpl'
-		),
-		array(
-			'template' => 'email/plain/order_cust.tpl',
-			'block' => 'email_plain_order_cust_basketitem',
-			'file' => 'views/blocks/email/plain/email_plain_order_cust_basketitem.tpl'
-		),
+	array(
+	    'template' => 'email/html/order_owner.tpl',
+	    'block' => 'email_html_order_owner_basketitem',
+	    'file' => 'views/blocks/email/html/email_html_order_owner_basketitem.tpl'
 	),
+	array(
+	    'template' => 'email/plain/order_owner.tpl',
+	    'block' => 'email_plain_order_ownerbasketitem',
+	    'file' => 'views/blocks/email/plain/email_plain_order_ownerbasketitem.tpl'
+	),
+	array(
+	    'template' => 'email/html/order_cust.tpl',
+	    'block' => 'email_html_order_cust_basketitem',
+	    'file' => 'views/blocks/email/html/email_html_order_cust_basketitem.tpl'
+	),
+	array(
+	    'template' => 'email/plain/order_cust.tpl',
+	    'block' => 'email_plain_order_cust_basketitem',
+	    'file' => 'views/blocks/email/plain/email_plain_order_cust_basketitem.tpl'
+	),
+    ),
 );
