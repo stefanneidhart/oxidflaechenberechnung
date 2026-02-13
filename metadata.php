136667b3ca71015254c7570502c40d65cc144e92
stefanneidhart <56339117+stefanneidhart@users.noreply.github.com>
2026-02-13 10:42:15 +0100

    Update metadata.php

--- metadata.php
+++ metadata.php
@@ -22,27 +22,25 @@ $aModule = array(
 	'en' => '<h2>Flächenberechung</h2>',
     ),
     'thumbnail' => 'calc.png',
-    'version' => '1.0.19',
+    'version' => '2.0.1',
     'author' => 'Stefan Neidhart',
     'url' => 'https://www.stefanneidhart.de',
     'email' => 'info@stefanneidhart.de',
     'extend' => array(
-	//\OxidEsales\Eshop\Core\ViewConfig::class => \sn\oxidflaechenberechnung\Core\ViewConfig::class,
-	\OxidEsales\Eshop\Application\Model\Article::class => \sn\oxidflaechenberechnung\Model\Article::class,
-	\OxidEsales\Eshop\Application\Model\Basket::class => \sn\oxidflaechenberechnung\Model\Basket::class,
-	\OxidEsales\Eshop\Application\Model\BasketItem::class => \sn\oxidflaechenberechnung\Model\BasketItem::class
-    //\OxidEsales\Eshop\Application\Model\OrderArticle::class => \sn\oxidflaechenberechnung\Model\OrderArticle::class,
+//	\OxidEsales\Eshop\Application\Model\Article::class => \sn\oxidflaechenberechnung\Model\Article::class,
+//	\OxidEsales\Eshop\Application\Model\Basket::class => \sn\oxidflaechenberechnung\Model\Basket::class,
+//	\OxidEsales\Eshop\Application\Model\BasketItem::class => \sn\oxidflaechenberechnung\Model\BasketItem::class
     ),
     'controllers' => array(
-	'maincontrollerflaechenberechnung' => \sn\oxidflaechenberechnung\Controller\Admin\MainController::class,
-	'ArticleSelectlistController' => \sn\oxidflaechenberechnung\Controller\ArticleSelectlistController::class,
+//	'maincontrollerflaechenberechnung' => \sn\oxidflaechenberechnung\Controller\Admin\MainController::class,
+//	'ArticleSelectlistController' => \sn\oxidflaechenberechnung\Controller\ArticleSelectlistController::class,
     ),
     'templates' => array(
 	'flaechenberechnungadmin.tpl' => 'sn/oxidflaechenberechnung/views/admin/tpl/flaechenberechnungadmin.tpl',
 	'snAjaxSelectlist.tpl' => 'sn/oxidflaechenberechnung/views/snAjaxSelectlist.tpl',
     ),
     'blocks' => array(
-	array(
+/*	array(
 	    'template' => 'page/details/inc/productmain.tpl',
 	    'block' => 'details_productmain_shortdesc',
 	    'file' => 'views/blocks/page/details/inc/details_productmain_flaechenberechnung.tpl'
@@ -81,6 +79,6 @@ $aModule = array(
 	    'template' => 'email/plain/order_cust.tpl',
 	    'block' => 'email_plain_order_cust_basketitem',
 	    'file' => 'views/blocks/email/plain/email_plain_order_cust_basketitem.tpl'
-	),
+	),*/
     ),
 );
